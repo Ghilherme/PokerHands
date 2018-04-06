26e0ce3e3193febac9f254d8a7694351535ddf48
Guilherme <g.p.oliveira@hotmail.com>
2018-04-05 21:36:40 -0300

    First Commit
    All the classes are working

---
+++ ConsoleApps/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApps
+{
+    class Card
+    {
+        public string suit;
+        public int value;
+
+        public Card(string v, string c)
+        {
+            convert(v);
+            suit = c;
+        }
+
+        //Convert the 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A to numbers;
+
+        public void convert(string x)
+        {
+            switch (x)
+            {
+                case "A":
+                    value = 14;
+                    return;
+                case "K":
+                    value = 13;
+                    return;
+
+                case "Q":
+                    value = 12;
+                    return;
+
+                case "J":
+                    value = 11;
+                    return;
+
+                case "T":
+                    value = 10;
+                    return;
+
+                default:
+                    value = int.Parse(x);
+                    return;
+            }
+        }
+    }
+}
---
+++ ConsoleApps/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the cards from the players' hands:");
+                string str = Console.ReadLine();
+            
+                Player black = new Player(str.Substring(0, 14));
+                Player white = new Player(str.Substring(15));
+                switch (black.compareTo(white))
+                {
+                    case 1:
+                        Console.WriteLine("Black wins.");
+                        break;
+                    case -1:
+                        Console.Write("White wins.");
+                        break;
+                    case 0:
+                        Console.WriteLine("Tie.");
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error has occurred: \n {ex}");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
